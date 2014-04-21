85c796fdb5328e13259a491f85a4b1bb7799f295
Gonzalo Hardy <hardy.gonzalo@gmail.com>
2014-04-21 10:27:19 -0300

    Fix iAds, github #13

--- Tasks/Classes/ViewControllers/TabBarController.m
+++ Tasks/Classes/ViewControllers/TabBarController.m
@@ -9,10 +9,14 @@
 #import "TabBarController.h"
 #import "UserSelectionViewController.h"
 #import "UsersModel.h"
+#import "iAd/ADBannerView.h"
 
-@interface TabBarController () {
-    IBOutlet NSLayoutConstraint *iAddsViewHeight;
+@interface TabBarController () <ADBannerViewDelegate> {
+    IBOutlet NSLayoutConstraint *iAdsViewHeight;
     IBOutlet UIView* iAdsView;
+    IBOutlet UIView* tabBar;
+    
+    CGFloat iAdsViewHeightValue;
 }
 
 @end
@@ -21,19 +25,34 @@
 
 - (void) viewDidLoad {
     [super viewDidLoad];
+    iAdsViewHeightValue = iAdsViewHeight.constant;
+    iAdsViewHeight.constant = 0;
+    iAdsView.hidden = YES;
     if ([UsersModel sharedInstance].purchasedAddsFree)
-        [self removeAdds];
+        [self performSelector:@selector(removeAdds) withObject:nil afterDelay:0.1];
 }
 
 - (void) removeAdds {
-    iAddsViewHeight.constant = 0;
+    iAdsViewHeight.constant = 0;
     iAdsView.hidden = YES;
+    
+    
 }
 
 - (void) changeUser {
     UserSelectionViewController* vc = [self.storyboard instantiateViewControllerWithIdentifier:@"UserSelectionViewController"];
     vc.isChangingUser = YES;
     [((UINavigationController*)self.destinationViewController) pushViewController:vc animated:YES];
+}
+
+
+#pragma mark - ADBannerViewDelegate Methods
+
+- (void)bannerViewDidLoadAd:(ADBannerView *)banner {
+    if (![UsersModel sharedInstance].purchasedAddsFree) {
+        iAdsView.hidden = NO;
+        iAdsViewHeight.constant = iAdsViewHeightValue;
+    }
 }
 
 @end
